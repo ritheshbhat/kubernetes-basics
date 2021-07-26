d0c1a527defb700512627674874e768c83f8d78f
Rithesh <rithesh@rithesh-Inspiron-5370>
2021-07-26 20:58:35 +0530

    2 deployment config for kubernetes cluster. custom image in ping-deployment.yml, deployment.yml is nginix

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
+
+
+from datetime import datetime
+
+x = datetime.now().min == 5
